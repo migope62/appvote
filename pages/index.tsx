7483c9cc8fd27c015969e90b7d1f430e21f1c101
Maxime Vande <Maxime62580@laposte.net>
2024-05-11 09:55:09 +0200

    Page-propose

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,29 @@ export default function Home() {
 
     return (
         <div>
-            <Header/>
-                <h1 className='h1accueil'>Bienvenue sur CitizenPulse</h1>
-                <div className="container">
-                    <h2>Sujets populaires</h2>
-                    <ul>
-                        <li><Link href="/vote/1">Un bilan psy gratuit  à 10 ans</Link></li>
-                        <li><Link href="/vote/2">Réglementation des réseaux sociaux</Link></li>
-                        <li><Link href="/vote/3">Réforme de l éducation</Link></li>
-                    </ul>
-                </div>        
-                <Footer/>        
+            <Header />
+            <h1 className='h1accueil'>Bienvenue sur CitizenPulse</h1>
+            <div className="container">
+                <h2>Sujets populaires</h2>
+                <ul>
+                    <li>Un bilan psy gratuit  à 10</li>
+                    <div className="button-group">
+                    <button type="submit"><span>oui</span></button>
+                    <button type="submit"><span>non</span></button>
+                    </div>
+                    <li>Réglementation des réseaux sociaux</li>
+                    <div className="button-group">
+                        <button type="submit"><span>oui</span></button>
+                        <button type="submit"><span>non</span></button>
+                    </div>
+                    <li>Réforme de l éducation</li>
+                    <div className="button-group">
+                        <button type="submit"><span>oui</span></button>
+                        <button type="submit"><span>non</span></button>
+                    </div>
+                </ul>
             </div>
+            <Footer />
+        </div>
     );
 }
